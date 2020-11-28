437f70dccf3e17fdbd94ff4765f04aa30f7a8a2f
Bartosz Stoliński <barf@krej.net>
2020-11-29 00:30:01 +0100

    3d projects

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Door : MonoBehaviour {
+    [SerializeField] Animator animator;
+    [SerializeField] UnityEvent onDoorOpen = new UnityEvent();
+    [SerializeField] UnityEvent onDoorClose = new UnityEvent();
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetBool("character_nearby", true);
+            onDoorOpen.Invoke();
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetBool("character_nearby", false);
+            onDoorClose.Invoke();
+        }
+    }
+}
